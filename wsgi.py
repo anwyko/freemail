e1fbe142eb6e256ad7b756785e9a8a59f3e04604
anwyko <anwyko.trim@gmail.com>
2022-04-13 16:33:47 -0400

    updates

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask.cli import with_appcontext, AppGroup
 
 from App.database import create_db
-from App.main import app, migrate
-from App.controllers import ( create_user, get_all_users_json, get_all_users, send_email )
+from App.main import app, migrate, populate_db
+from App.controllers import *
 
 # This commands file allow you to create convenient CLI commands
 # for testing controllers
@@ -14,6 +14,11 @@ from App.controllers import ( create_user, get_all_users_json, get_all_users, se
 def initialize():
     create_db(app)
     print('database intialized')
+
+@app.cli.command("pop", help="Populates the database")
+def populate():
+    populate_db()
+    print('database populated')
 
 '''
 USER COMMANDS
@@ -43,19 +48,99 @@ def list_user_command(format):
     else:
         print(get_all_users_json())
 
+@user_cli.command("add-email", help="Adds an email to a user")
+def add_email_command():
+    add_email_to_user()
+
+@user_cli.command("add-list", help="Adds an list to a user")
+def add_list_command():
+    add_list_to_user()
+
 app.cli.add_command(user_cli) # add the group to the cli
+
+
+
+
+'''
+Email Commands
+'''
+email_cli = AppGroup('email', help='User object commands') 
+
+@email_cli.command("create", help="Creates an email")
+@click.argument("subject", default="subject")
+@click.argument("body", default="body")
+@click.argument("status", default="draft")
+def create_email_command(subject, body, status):
+    create_email(subject, body, status)
+    print(f'email created!')
+
+@email_cli.command("send", help="ASends an email")
+def send_command():
+    send_email()
+
+@email_cli.command("send-bulk", help="ASends an email")
+def send_bulk_command():
+    send_bulk(2)
+
+@email_cli.command("add-list", help="Adds a recipient to a list")
+def add_list_command():
+    add_list_to_email()
+    # print(f'recip added!')
+
+app.cli.add_command(email_cli)
+
+
+
+
+'''
+RECIPIENT COMMANDS
+'''
+recip_cli = AppGroup('recip', help='User object commands') 
+
+@recip_cli.command("create", help="Creates a recipient")
+@click.argument("firstname", default="jane")
+@click.argument("lastname", default="doe")
+@click.argument("email", default="email")
+def create_recipient_command(firstname, lastname, email):
+    create_recipient(firstname, lastname, email)
+    print(f'recipient created!')
+
+app.cli.add_command(recip_cli)
+
+
+
+
+'''
+MAILINGLIST COMMANDS
+'''
+list_cli = AppGroup('list', help='User object commands') 
+
+@list_cli.command("create", help="Creates a list")
+@click.argument("name", default="list")
+def create_list_command(name):
+    create_list(name)
+    print(f'list created!')
+
+@list_cli.command("add-recip", help="Adds a recipient to a list")
+def add_list_command():
+    add_recipient_to_list(2, 7)
+    # print(f'recip added!')
+
+@list_cli.command("rem-recip", help="Adds a recipient to a list")
+def add_list_command():
+    remove_recipient_from_list(2, 3)
+    # print(f'recip added!')
+
+app.cli.add_command(list_cli)
+
+
 
 
 '''
 Generic Commands
 '''
-
-
 @app.cli.command("init")
 def initialize():
     create_db(app)
     print('database intialized')
 
-@app.cli.command("send")
-def send():
-    send_email()
