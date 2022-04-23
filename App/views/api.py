314483c8bfce92b3b11c204c089f278e5ed42856
Kareem <kareem.williams@my.uwi.edu>
2022-04-23 17:39:26 -0400

    hompeage.html render on site load

--- App/views/api.py
+++ App/views/api.py
@@ -9,7 +9,7 @@ api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 @api_views.route('/success', methods=['GET'])
 def success():
