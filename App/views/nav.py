606354b994671d9c08481cc98acfa71203d1ad9f
Kareem <kareem.williams@my.uwi.edu>
2022-04-22 20:55:45 -0400

    nav

--- App/views/nav.py
+++ App/views/nav.py
@@ -9,15 +9,15 @@ def get_list_doc():
 
 @nav_views.route('/sent', methods=['GET'])
 def get_sent_doc():
-    return render_template('list.html')
+    return render_template('sent.html')
 
 @nav_views.route('/draft', methods=['GET'])
 def get_draft_doc():
-    return render_template('list.html')
+    return render_template('draft.html')
 
 @nav_views.route('/contact', methods=['GET'])
 def get_contact_doc():
-    return render_template('list.html')
+    return render_template('contact.html')
 
 @nav_views.route('/account', methods=['GET'])
 def get_account_doc():
